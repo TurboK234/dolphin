bce040e7035a32ab7c7ae1af13ff9e042fa5ee56
Henrik Söderström <hosode@gmail.com>
2016-06-24 23:56:10 +0300

    TEV fix for older Intel HD drivers

--- Source/Core/VideoCommon/PixelShaderGen.cpp
+++ Source/Core/VideoCommon/PixelShaderGen.cpp
@@ -155,7 +155,7 @@ template <class T>
 static void WriteStage(T& out, pixel_shader_uid_data* uid_data, int n, API_TYPE ApiType,
                        const char swapModeTable[4][5]);
 template <class T>
-static void WriteTevRegular(T& out, const char* components, int bias, int op, int clamp, int shift);
+static void WriteTevRegular(T& out, const char* components, int bias, int op, int clamp, int shift, API_TYPE ApiType);
 template <class T>
 static void SampleTexture(T& out, const char* texcoords, const char* texswap, int texmap,
                           API_TYPE ApiType);
@@ -933,7 +933,7 @@ static void WriteStage(T& out, pixel_shader_uid_data* uid_data, int n, API_TYPE 
   out.Write("\t%s = clamp(", tevCOutputTable[cc.dest]);
   if (cc.bias != TEVBIAS_COMPARE)
   {
-    WriteTevRegular(out, "rgb", cc.bias, cc.op, cc.clamp, cc.shift);
+    WriteTevRegular(out, "rgb", cc.bias, cc.op, cc.clamp, cc.shift, ApiType);
   }
   else
   {
@@ -966,7 +966,7 @@ static void WriteStage(T& out, pixel_shader_uid_data* uid_data, int n, API_TYPE 
   out.Write("\t%s = clamp(", tevAOutputTable[ac.dest]);
   if (ac.bias != TEVBIAS_COMPARE)
   {
-    WriteTevRegular(out, "a", ac.bias, ac.op, ac.clamp, ac.shift);
+    WriteTevRegular(out, "a", ac.bias, ac.op, ac.clamp, ac.shift, ApiType);
   }
   else
   {
@@ -994,7 +994,7 @@ static void WriteStage(T& out, pixel_shader_uid_data* uid_data, int n, API_TYPE 
 }
 
 template <class T>
-static void WriteTevRegular(T& out, const char* components, int bias, int op, int clamp, int shift)
+static void WriteTevRegular(T& out, const char* components, int bias, int op, int clamp, int shift, API_TYPE ApiType)
 {
   const char* tevScaleTableLeft[] = {
       "",       // SCALE_1
@@ -1032,11 +1032,22 @@ static void WriteTevRegular(T& out, const char* components, int bias, int op, in
   // - c is scaled from 0..255 to 0..256, which allows dividing the result by 256 instead of 255
   // - if scale is bigger than one, it is moved inside the lerp calculation for increased accuracy
   // - a rounding bias is added before dividing by 256
+  // This version has an alternative expression for D3D to eliminate graphical glitches
+  // when using Intel HD 4000 (or lower) with old drivers.
   out.Write("(((tevin_d.%s%s)%s)", components, tevBiasTable[bias], tevScaleTableLeft[shift]);
   out.Write(" %s ", tevOpTable[op]);
-  out.Write("(((((tevin_a.%s<<8) + (tevin_b.%s-tevin_a.%s)*(tevin_c.%s+(tevin_c.%s>>7)))%s)%s)>>8)",
-            components, components, components, components, components, tevScaleTableLeft[shift],
-            tevLerpBias[2 * op + (shift != 3)]);
+  if (ApiType == API_D3D)
+  	{
+	  out.Write("((((tevin_a.%s*256 + (tevin_b.%s-tevin_a.%s)*(tevin_c.%s+(tevin_c.%s >= 128 ? 1 : 0)))%s)%s)>>8)",
+		components, components, components, components, components,
+		tevScaleTableLeft[shift], tevLerpBias[2 * op + (shift != 3)]);
+	}
+  else
+	{
+	  out.Write("((((tevin_a.%s*256 + (tevin_b.%s-tevin_a.%s)*(tevin_c.%s+(tevin_c.%s>>7)))%s)%s)>>8)",
+		components, components, components, components, components,
+		tevScaleTableLeft[shift], tevLerpBias[2 * op + (shift != 3)]);
+	}
   out.Write(")%s", tevScaleTableRight[shift]);
 }
 
